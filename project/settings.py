a6350431ac39c5926788b2541eb5a72bc00d1518
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-30 14:50:44 -0300

    preparando o ambiente para coleta de arquivos estáticos

--- project/settings.py
+++ project/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base' / 'static'
 ]
+
+STATIC_ROOT = BASE_DIR / 'static_files'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
